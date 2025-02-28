29bee9c05dcf21ad93ccb12d1393e903e3a25107
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2025-02-28 21:03:59 +0530

    login page init

---
+++ src/app/login/page.js
@@ -0,0 +1,26 @@
+"use client"
+import styles from "./loginpage.module.css"
+
+export default function Login() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.form}>
+                <form>
+                    <div className={styles.exit}>
+
+                    </div>
+
+                    <div className={styles.input}>
+                        <input></input>
+                        <input></input>
+                    </div>
+
+                    <div className={styles.continue}>
+
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
+}
